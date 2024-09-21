ee3367f03fbfcd1a81febfa1dbcb0cdd1eacea4e
Frederick Manyo <manyofrederick99@gmail.com>
2024-09-21 21:38:37 +0000

    first commit

---
+++ core/serializers.py
@@ -0,0 +1,15 @@
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import UserSerializer as BaseUserSerializer
+from . import models
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields = ['id', 'first_name', 'last_name',
+                  'email', 'password', 'phone',
+                  'residence', 'gps_address', 'occupation']
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = ['id', 'first_name', 'last_name',
+                  'email', 'password', 'phone',
+                  'residence', 'gps_address', 'occupation']
